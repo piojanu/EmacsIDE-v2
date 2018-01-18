ff7ac0c59e49da570d99b4ce8d7a2ea88ad64ce8
Piotr Januszewski <piojanuszewski@gmail.com>
2018-01-18 22:26:53 +0100

    Install exec-path-from-shell
    It ensures path consistency between shell and GUI emacs.

--- init.el
+++ init.el
@@ -205,6 +205,14 @@
 )
 
 
+;; Install exec-path-from-shell, for path consistency between shell and GUI emacs
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+)
+
+
 ;; Install Projectile
 (use-package projectile
   :diminish projectile-mode
