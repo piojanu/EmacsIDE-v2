0f09094e0d54b1a07021cbaf065e2b47fc9a2a48
Piotr Januszewski <piojanuszewski@gmail.com>
2018-01-18 20:31:40 +0100

    Install Magit and evil-magit

--- init.el
+++ init.el
@@ -150,6 +150,20 @@
 )
 
 
+;;;; GIT
+
+;; Install Magit
+(use-package magit
+  :commands magit-status
+  :bind
+  ("C-c g" . magit-status)
+  :config 
+  ;; Better integration between Evil and Magit
+  (use-package evil-magit)
+  :demand
+)
+
+
 ;;;; UTILITIES
 
 ;; Install Evil
@@ -184,7 +198,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (evil-magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
