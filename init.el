abf883433a970d1a52768632a183cfa2c1875305
Piotr Januszewski <piotr.januszewski@quantumlab.co>
2018-03-23 07:59:31 +0100

    Add function for listing TODOs in current org buffer

--- init.el
+++ init.el
@@ -237,6 +237,15 @@
   ;; Wrap lines
   (add-hook 'org-mode-hook #'toggle-truncate-lines)
 
+  ;; org-todo-list for current file
+  (defun org-todo-list-current-file (&optional arg)
+  "Like `org-todo-list', but using only the current buffer's file."
+  (interactive "P")
+  (let ((org-agenda-files (list (buffer-file-name (current-buffer)))))
+    (if (null (car org-agenda-files))
+        (error "%s is not visiting a file" (buffer-name (current-buffer)))
+      (org-todo-list arg))))
+
   ;; Add workflow states
   (setq org-todo-keywords
 	'((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
