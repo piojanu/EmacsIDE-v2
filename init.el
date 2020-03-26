3e9bae2db70b7b91dea94f3898d7f8c61bf6be40
Piotr Januszewski <piotr.januszewski@pg.edu.pl>
2020-03-26 10:54:19 +0100

    Remove conda env, it doesn't work with company

--- init.el
+++ init.el
@@ -283,21 +283,6 @@ There are two things you can do about this warning:
 )
 
 
-;;;; ENVIRONMENTS
-;;;;--------------
-
-
-;; Emacs library for working with conda environments
-(use-package conda
-  :config
-  ;; if you want interactive shell support, include:
-  (conda-env-initialize-interactive-shells)
-  ;; if you want eshell support, include:
-  (conda-env-initialize-eshell)
-  ;; if you want auto-activation (see below for details), include:
-  (conda-env-autoactivate-mode nil))
-
-
 ;;;; CODING
 ;;;;--------
 
